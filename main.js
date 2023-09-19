24417c5320e185b17b0a614d24d6e38205ffae8e
Njato <herilazanjato@gmail.com>
2023-09-19 12:19:53 +0300

    split text from js

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 import gsap from "gsap"
-gsap.from("span,i", {
+const text = document.querySelector('p')
+const split = (el) => {
+    const text = el.textContent;
+    const txtSplit = text.split('');
+    el.textContent = '';
+    for (let i = 0; i < txtSplit.length; i++) {
+        const span = document.createElement('span');
+        span.textContent = txtSplit[i];
+        el.appendChild(span)
+    }
+}
+split(text)
+gsap.from("p span", {
     duration: 2, repeat: -1, stagger: 0.3, opacity: 0
 })
